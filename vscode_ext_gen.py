08bf308cf434b6171e1f6662d4ca79b5e39c01b5
lakshmikanth ayyadevara <lakshmikanth2001@gmail.com>
2022-03-10 13:54:09 +0530

    vscode_ext_gen.py fix and adding .vscodeignore

--- vscode_ext_gen.py
+++ vscode_ext_gen.py
@@ -2,7 +2,7 @@ import json
 import sys
 
 """
-VS-Code Snippet Sytax
+VS-Code Snippet Syntax
     Example:
     "Print to console": {
         "prefix": "log",
@@ -16,28 +16,25 @@ VS-Code Snippet Sytax
 
 if __name__ == "__main__":
     """
-        This is a helper script to convert python or any launguage code to body of VS-Code snippet body"
-
-        Usage:
-            python vscode_ext_gen.py <file_path>
-
+        This is a helper script to convert python or any language code to body of VS-Code snippet body"
         This wont include tabstops and variables for snippet body
 
         Collect the body string from the stdout and past it to VS-Code snippet body
     """
+    default_file_path = "test.py"
     if len(sys.argv) > 1:
+        default_file_path = sys.argv[1]  # if argument is provided use the argument one
+    # end if
 
-        file_path = sys.argv[1]
-        with open(file_path, 'r') as code:
-            content = code.read()
-            body = []
-            for line in content.splitlines():
-                line = line.replace("    ", "\t")
+    try:
+        with open(default_file_path, 'r') as code:
+            for line in code:
+                # assume using 4 spaces as tab
+                line = line.replace("    ", "\t").strip()
                 line = json.dumps(line)
-                body.append(line)
-
-        for line in body:
-            print(line, ",")
-
-    else:
-        raise ValueError("pass file path as command line argument")
+                print(line, end=",\n")
+    except FileNotFoundError:
+        raise ValueError("""file not found, check command line argument of file path
+    Usage:
+        python vscode_ext_gen.py <file_path>
+    If <file_path> not specify, test.py will be the default file""")
